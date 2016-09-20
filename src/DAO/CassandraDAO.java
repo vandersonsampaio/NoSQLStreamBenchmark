a616d8ccfea5629f529d58ab9c93d3dc3360036a
Vanderson <vanderson.sampaio@yahoo.com.br>
2016-09-20 16:44:44 -0300

    Inclusão de dos JARs e criação de códigos de acesso de todos os bancos de dados.

--- src/DAO/CassandraDAO.java
+++ src/DAO/CassandraDAO.java
@@ -1,8 +1,19 @@
 package DAO;
 
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+
 public class CassandraDAO implements IDAO {
 	
-	//CassandraClient cassandra;
+	private Cluster cluster;
+	private Session session;
+	
+	public CassandraDAO(){
+		this.cluster = Cluster.builder().addContactPoint("178.62.254.52").build();
+		this.session = this.cluster.connect("demo");
+	}
 	
 	@Override
 	public byte[] obter(String resolucao){
@@ -11,6 +22,15 @@ public class CassandraDAO implements IDAO {
 	
 	@Override
 	public boolean inserir(String resolucao, byte[] dados){
+		session.execute("INSERT INTO users (lastname, age, city, email, firstname) VALUES ('Jones', 35, 'Austin', 'bob@example.com', 'Bob')");
+
+		ResultSet results = session.execute("SELECT * FROM users WHERE lastname='Jones'");
+		for (Row row : results) {
+			System.out.format("%s %d\n", row.getString("firstname"), row.getInt("age"));
+		}
+		
+		cluster.close();
+
 		return true;
 	}
 	
--- src/DAO/HBaseDAO.java
+++ src/DAO/HBaseDAO.java
@@ -1,8 +1,47 @@
 package DAO;
 
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.MasterNotRunningException;
+import org.apache.hadoop.hbase.ZooKeeperConnectionException;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+
+import com.google.protobuf.ServiceException;
+
 public class HBaseDAO implements IDAO {
 	
-	//HBase hbase;
+	private String HOST_CONNECTION = "178.62.254.52";
+	//private static Configuration conf = null;
+	
+	public HBaseDAO(){
+		Configuration conf = HBaseConfiguration.create();
+		conf.set("hbase.zookeeper.quorum", "178.62.254.52");
+		//this.conf.set("hbase.master", "178.62.254.52:60010");
+        conf.set("hbase.zookeeper.property.clientport", "2181");
+        
+        try {
+			HBaseAdmin.checkHBaseAvailable(conf);
+		} catch (MasterNotRunningException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ZooKeeperConnectionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 	@Override
 	public byte[] obter(String resolucao){
@@ -11,6 +50,7 @@ public class HBaseDAO implements IDAO {
 	
 	@Override
 	public boolean inserir(String resolucao, byte[] dados){
+		getAllRecord("test");
 		return true;
 	}
 	
@@ -18,4 +58,24 @@ public class HBaseDAO implements IDAO {
 	public void limpar(){
 		
 	}
+	
+	 public void getAllRecord (String tableName) {
+        /*try{
+        	HBaseAdmin admin = new HBaseAdmin(conf);
+             HTable table = new HTable(conf, tableName);
+             Scan s = new Scan();
+             ResultScanner ss = table.getScanner(s);
+             for(Result r:ss){
+                 for(KeyValue kv : r.raw()){
+                    System.out.print(new String(kv.getRow()) + " ");
+                    System.out.print(new String(kv.getFamily()) + ":");
+                    System.out.print(new String(kv.getQualifier()) + " ");
+                    System.out.print(kv.getTimestamp() + " ");
+                    System.out.println(new String(kv.getValue()));
+                 }
+             }
+        } catch (IOException e){
+            e.printStackTrace();
+        }*/
+    }
 }
