e4b6fc67625f6ce4ff52179cb6ade0d878efaabb
Vanderson <vanderson.sampaio@yahoo.com.br>
2016-09-28 11:48:34 -0300

    Conclusão de inclusão e retorno Voldemort.

--- src/DAO/HBaseDAO.java
+++ src/DAO/HBaseDAO.java
@@ -1,62 +1,138 @@
 package DAO;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.Charset;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
 public class HBaseDAO implements IDAO {
-	
+
 	private static final Charset UTF8_CHARSET = Charset.forName("UTF-8");
 	private String HOST_CONNECTION = "178.62.254.52";
 	private Client cliente;
-	
-	public HBaseDAO(){
-		this.cliente = Client.create(); 
+
+	private static Configuration config = HBaseConfiguration.create();
+
+	public HBaseDAO() {
+		config.set("hbase.zookeeper.quorum", "178.62.254.52");
+		config.set("hbase.zookeeper.property.clientPort", "2181");
+		config.set("hbase.master", "178.62.254.52:60000");
 	}
-	
+
 	@Override
-	public byte[] obter(String resolucao){
+	public byte[] obter(String resolucao) {
+		// HTable table;
+		try {
+			System.out.println("conexão");
+			Connection connection = ConnectionFactory.createConnection(config);
+			Table table = connection.getTable(TableName.valueOf("movie"));
+			System.out.println("tabela");
+			try {
+				// Use the table as needed, for a single operation and a single
+				// thread
+				System.out.println("consulta");
+				Get s = new Get("4k".getBytes());
+				System.out.println(table.getName());
+				Result ss = table.get(s);
+				System.out.println("Resultado");
+
+				for (KeyValue kv : ss.raw()) {
+					System.out.print(new String(kv.getRow()) + " ");
+					System.out.print(new String(kv.getFamily()) + ":");
+					System.out.print(new String(kv.getQualifier()) + " ");
+					System.out.print(kv.getTimestamp() + " ");
+					System.out.println(new String(kv.getValue()));
+				}
+
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				table.close();
+				connection.close();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
-	
-	@Override
-	public boolean inserir(String resolucao, byte[] dados){
+
+	public void get(final String paybackBarcode) {
+		WebResource service;
 		try {
-			WebResource service = cliente.resource("http://" + HOST_CONNECTION + ":8030/" +
-					URLEncoder.encode("test", UTF8_CHARSET.displayName()) + "/" +
-					URLEncoder.encode("movie", UTF8_CHARSET.displayName()) + "/" +
-					URLEncoder.encode("resolucao", UTF8_CHARSET.displayName()) + "/" +
-					URLEncoder.encode("240p", UTF8_CHARSET.displayName()));
-			
+			service = cliente
+					.resource("178.62.254.52:8030/"
+							+ URLEncoder.encode("movie",
+									UTF8_CHARSET.displayName())
+							+ "/"
+							+ URLEncoder.encode("4k",
+									UTF8_CHARSET.displayName()));
+
+			ClientResponse response = service.get(ClientResponse.class);
+			if (response.hasEntity() && response.getStatus() == 200) {
+				System.out.println(response.getEntity(String.class));
+			}
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public boolean inserir(String resolucao, byte[] dados) {
+		try {
+			WebResource service = cliente
+					.resource("http://"
+							+ HOST_CONNECTION
+							+ ":8030/"
+							+ URLEncoder.encode("test",
+									UTF8_CHARSET.displayName())
+							+ "/"
+							+ URLEncoder.encode("movie",
+									UTF8_CHARSET.displayName())
+							+ "/"
+							+ URLEncoder.encode("resolucao",
+									UTF8_CHARSET.displayName())
+							+ "/"
+							+ URLEncoder.encode("240p",
+									UTF8_CHARSET.displayName()));
+
 			ClientResponse response = service.put(ClientResponse.class);
-			
-			if(response.getStatus() == 200)
+
+			if (response.getStatus() == 200)
 				System.out.println("Deu certo!");
 			else
 				System.out.println("Deu errado: " + response.getStatus());
-			
+
 			return true;
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return false;
 		}
-		
-	}
-	
-	@Override
-	public void limpar(){
-		
-	}
-	
-	@Override
-	public void close(){
-		
-	}
-	
-}
 
+	}
+
+	@Override
+	public void limpar() {
+
+	}
+
+	@Override
+	public void close() {
+
+	}
+
+}
--- src/DAO/VoldemortDAO.java
+++ src/DAO/VoldemortDAO.java
@@ -6,41 +6,38 @@ import voldemort.client.StoreClient;
 import voldemort.client.StoreClientFactory;
 import voldemort.versioning.Versioned;
 
+import org.apache.log4j.Logger;
+
 public class VoldemortDAO implements IDAO {
-	
-	private String HOST_CONNECTION = "tcp://178.62.254.52:6666"; 
+
+	private String HOST_CONNECTION = "tcp://localhost:6666";
 	private StoreClientFactory factory;
-	StoreClient<String, String> client;
-	
-	public VoldemortDAO(){
-		//ClientConfig config = new ClientConfig().setBootstrapUrls(HOST_CONNECTION);
-		factory = new SocketStoreClientFactory(new ClientConfig().setBootstrapUrls(HOST_CONNECTION));
-		client = factory.getStoreClient("test");
+	StoreClient<String, byte[]> client;
+
+	public VoldemortDAO() {
+		this.factory = new SocketStoreClientFactory(
+				new ClientConfig().setBootstrapUrls(HOST_CONNECTION));
+		this.client = this.factory.getStoreClient("test");
 	}
-	
+
 	@Override
-	public byte[] obter(String resolucao){
-		String retorno1 = client.get("hello").toString();
-		Versioned<String> retorno2 = client.get("some_key");
-		return null;
+	public byte[] obter(String resolucao) {
+		return client.getValue(resolucao);
 	}
-	
+
 	@Override
-	public boolean inserir(String resolucao, byte[] dados){
-		String retorno1 = client.get("hello").toString();
-		Versioned<String> retorno2 = client.get("some_key");
-		retorno2.setObject("some_value");
-		client.put("some_key", retorno2);
+	public boolean inserir(String resolucao, byte[] dados) {
+		client.put(resolucao, dados);
 		return true;
 	}
-	
+
 	@Override
-	public void close(){
+	public void close() {
 		this.factory.close();
 	}
-	
+
 	@Override
-	public void limpar(){
-		
+	public void limpar() {
+
 	}
 }
